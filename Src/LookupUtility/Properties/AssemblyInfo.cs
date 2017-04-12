d43f81c912b5549c48a4139c85239b17a22fdd83
Robin Hultman <robin.hultman@gmail.com>
2017-04-12 13:23:52 +0200

    Msi generation fixes

--- Src/LookupUtility/Properties/AssemblyInfo.cs
+++ Src/LookupUtility/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shared.Utilities.LookupUtility")]
+[assembly: AssemblyTitle("BizTalkComponents.Utilities.LookupUtility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shared.Utilities.LookupUtility")]
+[assembly: AssemblyProduct("BizTalkComponents.Utilities.LookupUtility")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
