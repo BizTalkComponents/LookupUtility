cf7e90f41b318302637763fc63f820312fee21f6
robinhultman <robin.hultman@gmail.com>
2018-08-27 10:47:16 +0200

    Added a mock service for testing

---
+++ Src/LookupUtility/Application/MockApplicationService.cs
@@ -0,0 +1,50 @@
+﻿using BizTalkComponents.Utilities.LookupUtility.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizTalkComponents.Utilities.LookupUtility.Application
+{
+    public class MockApplicationService : IApplicationService
+    {
+        private LookupUtilityService _svc = null;
+
+        public MockApplicationService()
+        {
+            _svc = new LookupUtilityService(new LookupRepositoryMock());
+        }
+        public string GetValue(string list, string key, bool throwIfNotExists = false, bool allowDefaults = false)
+        {
+            string value;
+
+            try
+            {
+                value = _svc.GetValue(list, key, throwIfNotExists, allowDefaults);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return value;
+        }
+
+        public string GetValue(string list, string key, string defaultValue)
+        {
+            string value;
+
+            try
+            {
+                value = _svc.GetValue(list, key, defaultValue);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return value;
+        }
+    }
+}
